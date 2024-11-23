ff575e1bc848b32418415fc93cb9593ab73c4515
HasithDeAlwis <hasithde24@gmail.com>
2024-11-23 00:41:38 -0500

    chore: create entrypoint for deals

---
+++ src/lib/features/deals/deals.presenter.svelte
@@ -0,0 +1,9 @@
+<script lang="ts">
+	import type { Deal } from '$lib/entities/deal';
+	import DealCarousel from './deal-carousel/ui/deal-carousel.component.svelte';
+	let { category, deals }: { category: string; deals: Deal[] } = $props();
+</script>
+
+<section class="mx-auto max-w-screen-xl p-4">
+	<DealCarousel {category} {deals} />
+</section>
