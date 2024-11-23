f1b2854592adbe4ca68724418f6c2927f4e72afd
HasithDeAlwis <hasithde24@gmail.com>
2024-11-23 00:07:44 -0500

    feat(lib/features/deals): create carousel

---
+++ src/lib/features/deals/deal-carousel/ui/deal-carousel.component.svelte
@@ -0,0 +1,17 @@
+<script lang="ts">
+	import type { Deal } from '$lib/entities/deal';
+	import { ScrollArea } from '$lib/shadcn/ui/scroll-area';
+	import DealCard from '$lib/features/deals/deal-card/ui/deal.card.component.svelte';
+	let { category, deals }: { category: string; deals: Deal[] } = $props();
+</script>
+
+<div class="flex flex-col gap-y-2">
+	<h1 class="text-3xl font-bold underline">{category}</h1>
+	<ScrollArea class="flex h-auto w-full" scroll orientation="horizontal">
+		<div class="flex flex-row gap-x-4">
+			{#each deals as deal}
+				<DealCard {deal} />
+			{/each}
+		</div>
+	</ScrollArea>
+</div>
