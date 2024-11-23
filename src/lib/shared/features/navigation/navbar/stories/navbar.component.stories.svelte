af18ca2b31170e9b0f29283e9545be2ad3b2b720
HasithDeAlwis <hasithde24@gmail.com>
2024-11-22 22:10:07 -0500

    feat(lib/shared/feature/navigation/navbar): create component

---
+++ src/lib/shared/features/navigation/navbar/stories/navbar.component.stories.svelte
@@ -0,0 +1,26 @@
+<script module lang="ts">
+	import { defineMeta } from '@storybook/addon-svelte-csf';
+	import Navbar from '../ui/navbar.component.svelte';
+
+	const { Story } = defineMeta({
+		title: 'Components/Navbar',
+		component: Navbar,
+		tags: ['autodocs'],
+		parameters: {
+			layout: 'fullscreen'
+		},
+		args: {
+			location: 'Kingston, ON'
+		},
+		argTypes: {
+			location: {
+				control: { type: 'text' },
+				description: 'Current page location displayed in the navbar.'
+			}
+		}
+	});
+</script>
+
+<!-- Stories -->
+
+<Story name="Home Navbar" args={{ location: 'Kingston, ON' }} />
