20b5dc7319b581d99800c2b5feead6debc6a230f
HasithDeAlwis <hasithde24@gmail.com>
2024-11-22 22:10:28 -0500

    feat(routes): add navbar to all routes

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,6 +1,12 @@
 <script lang="ts">
 	import '../app.css';
+	// TODO: Fix so that we can use barreled imports
+	import Navbar from '$lib/shared/features/navigation/navbar/ui/navbar.presenter.svelte';
+
+	// TODO: Replace with logic to fetch user location --> local storage :)
+	const location = 'Kingston, ON';
 	let { children } = $props();
 </script>
 
+<Navbar {location} />
 {@render children()}
