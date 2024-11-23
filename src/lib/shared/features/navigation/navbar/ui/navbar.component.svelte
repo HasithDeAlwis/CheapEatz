d054b649d862571733cac47f03729888b5ef361d
HasithDeAlwis <hasithde24@gmail.com>
2024-11-22 22:10:07 -0500

    feat(lib/shared/feature/navigation/navbar): create component

---
+++ src/lib/shared/features/navigation/navbar/ui/navbar.component.svelte
@@ -0,0 +1,40 @@
+<script lang="ts">
+	import { Input } from '$lib/shadcn/ui/input';
+	import * as Avatar from '$lib/shadcn/ui/avatar';
+	/**SCAFFOLDING
+	 * 1. Props for the navbar are imported here
+	 * 2. Props passed to the navbar component
+	 *
+	 */
+	// TODO: Implemente options later on with search
+	//let { options, location } = $props();
+	let { location } = $props();
+</script>
+
+<nav class="flex flex-col justify-between gap-y-3 bg-primary p-3 md:flex-row">
+	<div class="flex flex-col items-start gap-x-3 md:flex-row md:items-end">
+		<div class="flex w-full flex-row justify-between">
+			<a href="/">
+				<h2 class="text-3xl font-bold text-primary-foreground">CheapEatz</h2>
+			</a>
+			<div class="block md:hidden">
+				<Avatar.Root>
+					<Avatar.Image src="https://github.com/shadcn.png" alt="@shadcn" />
+					<Avatar.Fallback>CN</Avatar.Fallback>
+				</Avatar.Root>
+			</div>
+		</div>
+		<p class="w-full font-bold text-primary-foreground underline">{location}</p>
+	</div>
+	<div class="flex basis-1/2 flex-row gap-x-3">
+		<Input class="basis-full" placeholder="Search" />
+
+		<!--PROFILE ICON-->
+		<div class="hidden md:block">
+			<Avatar.Root>
+				<Avatar.Image src="https://github.com/shadcn.png" alt="@shadcn" />
+				<Avatar.Fallback>CN</Avatar.Fallback>
+			</Avatar.Root>
+		</div>
+	</div>
+</nav>
---
+++ src/lib/shared/features/navigation/navbar/ui/navbar.presenter.svelte
@@ -0,0 +1,15 @@
+<script lang="ts">
+	import NavbarComponent from './navbar.component.svelte';
+	/**SCAFFOLDING
+	 * 1. Props for the navbar are imported here
+	 * 2. Props passed to the navbar component
+	 *
+	 */
+	let { location }: { location: string } = $props();
+</script>
+
+<header class="w-full">
+	<div class="max-w-screen-3xl">
+		<NavbarComponent {location} />
+	</div>
+</header>
