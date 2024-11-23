3fabb30aa4329bad8129463452811ec7c7e8e901
HasithDeAlwis <hasithde24@gmail.com>
2024-11-22 23:32:04 -0500

    feat(lib/features/deals): create deal-item

---
+++ src/lib/features/deals/deal-card/ui/saved-tag.component.svelte
@@ -0,0 +1,10 @@
+<script lang="ts">
+	let { savedPercentage, className }: { savedPercentage: number; className: string } = $props();
+	import { cn } from '$lib/utils.js';
+</script>
+
+<div class={cn('rounded-xl border-border bg-secondary p-1', className)}>
+	<p class="text-sm text-secondary-foreground lg:text-base">
+		️‍🔥 Saved <span class="text-primary">{savedPercentage}</span>%
+	</p>
+</div>
---
+++ src/lib/features/deals/deal-card/ui/store-tag.component.svelte
@@ -0,0 +1,7 @@
+<script>
+	let { storeName } = $props();
+</script>
+
+<div class="inline-block rounded-3xl bg-slate-200 px-2 pt-[0.5]">
+	<p class="p-1 text-sm font-thin text-slate-800">{storeName}</p>
+</div>
